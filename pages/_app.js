df9417ed8a81d2e9ba7c56fbb518c61c3263f854
Suyog1002 <suyogambadare1003@gmail.com>
2023-08-12 23:40:17 +0530

    created layout, Navbar and Footer  Component

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import Layout from '../components/Layout';
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  
+  )
 }
