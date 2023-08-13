d11e74da33dc33773267f5d72c596fc029e72e83
Suyog1002 <suyogambadare1003@gmail.com>
2023-08-14 01:05:31 +0530

    created view page for product and recommending product list

--- components/Product.jsx
+++ components/Product.jsx
@@ -15,6 +15,7 @@ const Product = ({ product: {image,name,slug,price} }) => {
                     />
                     <p className='product-name'>{name}</p>
                     <p className='product-price'>${price}</p>
+                    
                 </div>
             </Link>
         </div>
