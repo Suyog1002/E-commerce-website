830198ccba0ac1a3bc204bf6ab746e709bac5538
Suyog1002 <suyogambadare1003@gmail.com>
2023-08-17 18:35:55 +0530

    Added confetti to the success page

--- pages/success.js
+++ pages/success.js
@@ -1,8 +1,9 @@
-import React, {useState,useEffect} from 'react'
-import Link from 'next/link'
-import {BsBagCheckFill} from 'react-icons/bs'
+import React, {useState,useEffect} from 'react';
+import Link from 'next/link';
+import {BsBagCheckFill} from 'react-icons/bs';
+import { useStateContext} from '../context/StateContext';
+import { runFireworks } from '../lib/utils';
 
-import { useStateContext} from '../context/StateContext'
 
 const Success = () => {
     const {setCartItems,setTotalPrice,setTotalQuantities} = useStateContext();
@@ -12,6 +13,7 @@ const Success = () => {
         setCartItems([]);
         setTotalPrice(0);
         setTotalQuantities(0);
+        runFireworks();
     },[]);
 
   return (
