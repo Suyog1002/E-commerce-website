8cfeed422ac671f934ded5f84e8e3d965c55d8cd
Suyog1002 <141540723+Suyog1002@users.noreply.github.com>
2023-10-22 00:54:27 +0530

    Update [slug].js

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -7,7 +7,11 @@ import { useStateContext } from '../../context/StateContext';
 const ProductDetails = ({product,products}) => {
     const {image,name,details,price}= product;
     const [index,setIndex] = useState(0);
-    const {decQty, incQty, qty,onAdd} = useStateContext();
+    const {decQty, incQty, qty,onAdd, setShowCart} = useStateContext();
+    const handleBuyNow = () => {
+        onAdd(product,qty);
+        setShowCart(true);
+    }
   return (
     <div> 
         <div className='product-detail-container'>
@@ -55,7 +59,7 @@ const ProductDetails = ({product,products}) => {
                 </div>
                 <div className='buttons'>
                     <button type='buttton' className='add-to-cart' onClick={() => onAdd(product,qty)}>Add to Cart</button>
-                    <button type='buttton' className='buy-now' onClick="">Buy Now</button>
+                    <button type='buttton' className='buy-now' onClick={handleBuyNow}>Buy Now</button>
                 </div>
             </div>
         </div>
